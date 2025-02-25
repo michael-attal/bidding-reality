238ad8c1aeb54d94c5666c5f9b82c68aae567594
Chachoune963 <sacha@toutut.fr>
2025-02-25 18:49:19 +0100

    (Finally) Lobby connection

--- Assets/Scripts/UI/ItemLobbiesPresenter.cs
+++ Assets/Scripts/UI/ItemLobbiesPresenter.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 using XRMultiplayer;
 
@@ -7,6 +9,8 @@ public class ItemLobbiesPresenter : MonoBehaviour
 {
     [SerializeField] private ItemLobbyItem itemLobbyPrefab;
     [SerializeField] private Transform itemLobbyContainer;
+
+    private Lobby currentLobby;
     
     void Start()
     {
@@ -19,7 +23,42 @@ public class ItemLobbiesPresenter : MonoBehaviour
         {
             var lobbyItem = Instantiate(itemLobbyPrefab, itemLobbyContainer);
             StartCoroutine(lobbyItem.Initialize(item));
-            lobbyItem.OnButtonClicked.AddListener(() => Debug.Log($"Go To {item.name} lobby"));
+            lobbyItem.OnButtonClicked.AddListener(() => JoinAuction(item.id));
+        }
+    }
+
+    // Find and join the corresponding auction lobby. If there is none, create one yourself.
+    private async void JoinAuction(string itemId)
+    {
+        XRINetworkGameManager.Connected.Subscribe(OnConnected);
+
+        var lobbies = await LobbyManager.GetLobbiesAsync();
+        bool foundLobby = false;
+        foreach (var lobby in lobbies.Results)
+        {
+            if (foundLobby || lobby.Data["itemId"].Value != itemId)
+                continue;
+
+            foundLobby = true;
+            XRINetworkGameManager.Instance.JoinLobbySpecific(lobby);
+            currentLobby = lobby;
+        }
+        
+        if (foundLobby)
+            return;
+        
+        Debug.Log("Couldn't find a lobby. Creating one now...");
+        var createdLobby = await XRINetworkGameManager.Instance.lobbyManager.CreateLobby();
+        createdLobby.Data["itemId"] = new DataObject(DataObject.VisibilityOptions.Public, itemId);
+        XRINetworkGameManager.Instance.JoinLobbySpecific(createdLobby);
+        currentLobby = createdLobby;
+    }
+
+    private void OnConnected(bool success)
+    {
+        if (success)
+        {
+            Debug.Log($"Connected successfully. ItemID: {currentLobby.Data["itemId"].Value}");
         }
     }
 }
